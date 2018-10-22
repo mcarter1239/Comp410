770279dcbb405f327c5a916cb4170e09e155c0bb
Michael <Michael@Michael-PC.mit.edu>
2018-10-22 15:56:48 -0400

    initial finish

--- mtc1239_assignment3/src/MinBinHeap_A3/MinBinHeap.java
+++ mtc1239_assignment3/src/MinBinHeap_A3/MinBinHeap.java
@@ -3,6 +3,7 @@ package MinBinHeap_A3;
 public class MinBinHeap implements Heap_Interface {
 	private EntryPair[] array; // load this array
 	private int size;
+	private int last;
 	private static final int arraySize = 10000; // Everything in the array will initially
 												// be null. This is ok! Just build out
 												// from array[1]
@@ -22,31 +23,133 @@ public class MinBinHeap implements Heap_Interface {
 
 	@Override
 	public void insert(EntryPair entry) {
-		// TODO Auto-generated method stub
+		this.last++;
+		this.array[this.last] = entry;
+		int n = this.last;
+		int p = this.PN(n);
+		EntryPair temp;
+		while ((p != 0) && (entry.priority < this.array[p].priority)) {
+			temp = this.array[n];
+			this.array[n] = this.array[p];
+			this.array[p] = temp;
+			n = p;
+			p = this.PN(n);
+		}
 
 	}
 
 	@Override
 	public void delMin() {
-		// TODO Auto-generated method stub
-
+		if (this.size() == 0)
+			return;
+		if (this.size() == 1) {
+			this.array[1] = null;
+			this.last--;
+			return;
+		}
+		this.array[1] = this.array[this.last];
+		this.array[this.last] = null;
+		this.last--;
+		// bubble down from root
+		EntryPair temp = null;
+		boolean done = false;
+		int n = 1;
+		int c = 0;
+		while (!done) {
+			if (this.isLeaf(n)) {
+				done = true;
+			} else {
+				if (this.hasOnlyLC(n)) {
+					c = this.LC(n);
+				} else {
+					c = (this.LCV(n) < this.RCV(n)) ? this.LC(n) : this.RC(n);
+				}
+				if (this.array[n].priority > this.array[c].priority) {
+					temp = this.array[c];
+					this.array[c] = this.array[n];
+					this.array[n] = temp;
+					n = c;
+				} else {
+					done = true;
+				}
+			}
+		}
 	}
 
 	@Override
 	public EntryPair getMin() {
-		// TODO Auto-generated method stub
-		return null;
+		return array[1];
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == null) {
+				return i - 1;
+			}
+		}
+		return array.length - 1;
 	}
 
 	@Override
 	public void build(EntryPair[] entries) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < entries.length; i++) {
+			array[i + 1] = entries[i];
+		}
+		this.last = entries.length - 1;
+		int sn = this.PN(last);
+		for (int k = sn; k > 0; k--) {
+			EntryPair temp = null;
+			boolean done = false;
+			int n = k;
+			int c = 0;
+			while (!done) {
+				if (this.isLeaf(n)) {
+					done = true;
+				} else if (this.hasOnlyLC(n)) {
+					c = this.LC(n);
+				} else {
+					c = (this.LCV(n) < this.RCV(n)) ? this.LC(n) : this.RC(n);
+				}
+				if (this.array[n].priority > this.array[c].priority) {
+					temp = this.array[n];
+					this.array[n] = this.array[c];
+					this.array[c] = temp;
+					n = c;
+				} else {
+					done = true;
+				}
 
+			}
+		}
 	}
+
+	public int LC(int n) {
+		return 2 * n;
+	}
+
+	public int RC(int n) {
+		return (2 * n) + 1;
+	}
+
+	public int PN(int n) {
+		return (int) Math.floor(n / 2);
+	}
+
+	public boolean hasOnlyLC(int n) {
+		return (this.RC(n) > this.last && this.LC(n) <= this.last);
+	}
+
+	private boolean isLeaf(int n) {
+		return (this.LC(n) > this.last && this.RC(n) > this.last);
+	}
+
+	private int RCV(int n) {
+		return array[RC(n)].priority;
+	}
+
+	private int LCV(int n) {
+		return array[LC(n)].priority;
+	}
+
 }
